276060c524f56519c7a5c520a1253f3b87858938
Akash Singh <akashgurjar6266@gmail.com>
2025-09-21 02:25:09 +0530

    initialize the function in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
- import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
   return (
-    <>
-      <h1>Hello Akash Singh Gurjar (Virana)</h1>
-    </>
+    <h1>Hello ! Gurjar Akash Singh Virana...</h1>
   )
 }
 
-export default App
+export default App;

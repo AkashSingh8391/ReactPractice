4cad39e5fdf79491697ebd4d38fba990561c554c
Akash Singh <akashgurjar6266@gmail.com>
2025-09-22 01:55:47 +0530

    added a component in App.jsx file(functional component)

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,27 @@ function App(){
   return (
     <div>
       <Header/>
-      <root/>
+      
     <h1>Hello ! Gurjar Akash Singh Virana...</h1>
 
+    <Apple/>
+    <Color/>
     </div>
+  )
+}
+
+
+function Apple(){
+  return (
+
+<h2>New function add via component</h2>
+
+  )
+}
+
+function Color(){
+  return (
+    <h2>color is red.</h2>
   )
 }
 
